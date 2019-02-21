efcb7032249e96884c16815aa9e7a9b47d11b1db
realyemin <realyemin@hotmail.com>
2019-02-21 15:16:16 +0300

    Update baglantim.php

--- baglantim.php
+++ baglantim.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION))
 //error_reporting(0);
 try {
 		
-	$db = new PDO("mysql:host=localhost;dbname=db_kelimeoyunu", "root", "nichtwar");
+	$db = new PDO("mysql:host=localhost;dbname=db_kelimeoyunu", "root", "");
 	 	$db->exec("SET NAMES 'utf8'; SET CHARSET 'utf8'");
 		} catch ( PDOException $e ){
 			print $e->getMessage();
